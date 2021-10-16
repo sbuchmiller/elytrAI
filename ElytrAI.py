f63d89716003bb5e0fd588833196292f972fcac1
sbuchmiller <scottbuchmiller@gmail.com>
2021-10-15 17:20:33 -0700

    sbuchmiller - added block in sky and started agent on block

--- ElytrAI.py
+++ ElytrAI.py
@@ -51,34 +51,20 @@ def GetMissionXML(seed, gp, size=10):
               <ServerHandlers>
                   <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"/>
                   <DrawingDecorator>
-                    <DrawSphere x="-27" y="70" z="0" radius="30" type="air"/>
+                    <DrawBlock x="0" y="70" z="0" type="stone"/>
                   </DrawingDecorator>
-                  <MazeDecorator>
-                    <Seed>'''+str(seed)+'''</Seed>
-                    <SizeAndPosition width="''' + str(size) + '''" length="''' + str(size) + '''" height="10" xOrigin="-32" yOrigin="69" zOrigin="-5"/>
-                    <StartBlock type="emerald_block" fixedToEdge="true"/>
-                    <EndBlock type="redstone_block" fixedToEdge="true"/>
-                    <PathBlock type="diamond_block"/>
-                    <FloorBlock type="air"/>
-                    <GapBlock type="air"/>
-                    <GapProbability>'''+str(gp)+'''</GapProbability>
-                    <AllowDiagonalMovement>false</AllowDiagonalMovement>
-                  </MazeDecorator>
                   <ServerQuitFromTimeUp timeLimitMs="10000"/>
                   <ServerQuitWhenAnyAgentFinishes/>
                 </ServerHandlers>
               </ServerSection>
 
-              <AgentSection mode="Survival">
-                <Name>CS175AwesomeMazeBot</Name>
+              <AgentSection mode="Creative">
+                <Name>elytrAI</Name>
                 <AgentStart>
-                    <Placement x="0.5" y="56.0" z="0.5" yaw="0"/>
+                    <Placement x="0.5" y="71" z="0.5" yaw="0"/>
                 </AgentStart>
                 <AgentHandlers>
                     <DiscreteMovementCommands/>
-                    <AgentQuitFromTouchingBlockType>
-                        <Block type="redstone_block"/>
-                    </AgentQuitFromTouchingBlockType>
                     <ObservationFromGrid>
                       <Grid name="floorAll">
                         <min x="-10" y="-1" z="-10"/>
@@ -88,7 +74,6 @@ def GetMissionXML(seed, gp, size=10):
                 </AgentHandlers>
               </AgentSection>
             </Mission>'''
-
 
 def load_grid(world_state):
     """
