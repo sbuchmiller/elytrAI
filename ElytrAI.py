f758550aadf13e4cd97b9a92d5f27089fb6bd51d
alecgcrane <alecgcrane@gmail.com>
2021-10-27 17:45:33 -0700

    Alec - Got it to run episode after episode
    I did what I needed to to get the program to run using the trainer. Step is still bare and it doesn't want to fly

--- ElytrAI.py
+++ ElytrAI.py
@@ -15,30 +15,42 @@ from priority_dict import priorityDictionary as PQ
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import randint
-import gym, ray
+import gym
+import ray
 from gym.spaces import Discrete, Box
-from ray.rllib.agents import a2c
+from ray.rllib.agents import ppo
+# from ray.rllib.agents import a2c
+
 
 # sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-class elytraFlyer(gym.env):
+class elytraFlyer(gym.Env):
 
-    def __init__(self,degreeChange=5):
+    def __init__(self, degreeChange=5):
         self.numObservations = 6
-        self.actionList = self.getPotentialActions(degreeChange)
+        self.actionDict = self.getPotentialActions()
+        self.log_frequency = 1
 
-        #RLlib params
-        self.action_space = Discrete(len(self.action_dict))
-        self.observation_space = Box(0,360, shape=(self.numObservations,), dtype=np.float32)
+        # RLlib params
+        self.action_space = Discrete(len(self.actionDict))
+        self.observation_space = Box(0, 1000, shape=(self.numObservations,), dtype=np.float32)
 
+        # Malmo Parameters
+        self.agent_host = MalmoPython.AgentHost()
+        try:
+            self.agent_host.parse(sys.argv)
+        except RuntimeError as e:
+            print('ERROR:', e)
+            print(self.agent_host.getUsage())
+            exit(1)
 
-        #agent parameters
+        # agent parameters
+        self.obs = None
         self.lastx = 0
         self.lasty = 0
         self.lastz = 0
         self.xvelocity = 0
         self.yvelocity = 0
         self.zvelocity = 0
-
 
         self.episodeStep = 0
         self.episodeReturn = 0
@@ -47,38 +59,49 @@ class elytraFlyer(gym.env):
         self.episodes = []
         self.flightDistances = []
 
-
     def reset(self):
-        '''
-            clear all per-episode variables and reset world for next episode
-        '''
-        #resets malmo world to xml file
+        """
+        Clear all per-episode variables and reset world for next episode
+        """
+        # resets malmo world to xml file
         world_state = self.init_malmo()
 
-        #logs episode number and returns
-        self.returns.append(self.episode_return)
+        # logs episode number and returns
+        self.returns.append(self.episodeReturn)
         currentStep = self.steps[-1] if len(self.steps) > 0 else 0
         self.steps.append(currentStep + self.episodeStep)
         self.episodeReturn = 0
         self.episodeStep = 0
         self.clearObservationVariables()
 
+        self.obs = self.get_observation(world_state)
+
+        return self.obs
 
     def step(self, action):
+        # choose action
+        print(f"Action: {action}")
 
-        #take observation
+        # take observation
+        world_state = self.agent_host.getWorldState()
+        for error in world_state.errors:
+            print("Error:", error.text)
+        print(world_state)
+        self.obs = self.get_observation(world_state)
 
-        #choose action
+        # Get Reward
+        reward = 0
+        for r in world_state.rewards:
+            reward += r.getValue()
+        self.episodeReturn += reward
 
-        #get reward
+        # check if mission ended
+        done = not world_state.is_mission_running
 
-        #check if mission ended
-
-
-        return ()
+        return self.obs, reward, done, dict()
 
 
-    def GetMissionXML(seed, gp, size=10):
+    def GetMissionXML(self):
         return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                 <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 
@@ -95,11 +118,10 @@ class elytraFlyer(gym.env):
                         <Weather>clear</Weather>
                     </ServerInitialConditions>
                     <ServerHandlers>
-                        <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"/>
+                        <FlatWorldGenerator generatorString="2;57;1;"/>
                         <DrawingDecorator>
                                 <DrawBlock x="0" y="100" z="0" type="lapis_block"/>
                         </DrawingDecorator>
-                        <ServerQuitFromTimeUp timeLimitMs="10000"/>
                         <ServerQuitWhenAnyAgentFinishes/>
                     </ServerHandlers>
                 </ServerSection>
@@ -116,47 +138,19 @@ class elytraFlyer(gym.env):
                         <HumanLevelCommands/>
                         <AbsoluteMovementCommands/>
                         <InventoryCommands/>
-                        <ObservationFromGrid>
-                            <Grid name="floorAll">
-                                <min x="-10" y="-1" z="-10"/>
-                                <max x="10" y="-1" z="10"/>
-                            </Grid>
-                        </ObservationFromGrid>
-                    </AgentHandlers>
+                        <ObservationFromFullStats/>
+                        <ObservationFromRay/>
+                        <AgentQuitFromTouchingBlockType>
+                            <Block type="diamond_block" />
+                        </AgentQuitFromTouchingBlockType>
+                        </AgentHandlers>
                 </AgentSection>
-                </Mission>'''
-
-    def load_grid(world_state):
-        """
-        Used the agent observation API to get a 21 X 21 grid box around the agent (the agent is in the middle).
-
-        Args
-            world_state:    <object>    current agent world state
-
-        Returns
-            grid:   <list>  the world grid blocks represented as a list of blocks (see Tutorial.pdf)
-        """
-        while world_state.is_mission_running:
-            #sys.stdout.write(".")
-            time.sleep(0.1)
-            world_state = agent_host.getWorldState()
-            if len(world_state.errors) > 0:
-                raise AssertionError('Could not load grid.')
-
-            if world_state.number_of_observations_since_last_state > 0:
-                msg = world_state.observations[-1].text
-                observations = json.loads(msg)
-                grid = observations.get(u'floorAll', 0)
-                break
-        return grid
+                </Mission>
+                '''
 
     def log_returns(self):
         """
         Log the current returns as a graph and text file
-
-        Args:
-            steps (list): list of global steps after each episode
-            returns (list): list of total return of each episode
         """
         try:
             box = np.ones(self.log_frequency) / self.log_frequency
@@ -188,21 +182,75 @@ class elytraFlyer(gym.env):
         except:
             print("unable to log flight distance results")
 
-    def getPotentialActions(self, degreeChange = 5):
-        '''
+    def init_malmo(self):
+        """
+        Initialize new malmo mission.
+        """
+        my_mission = MalmoPython.MissionSpec(self.GetMissionXML(), True)
+        my_mission_record = MalmoPython.MissionRecordSpec()
+        my_mission.requestVideo(800, 500)
+        my_mission.setViewpoint(1)
+        # Attempt to start a mission:
+        max_retries = 3
+        my_clients = MalmoPython.ClientPool()
+        my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000))  # add Minecraft machines here as available
+
+        for retry in range(max_retries):
+            try:
+                self.agent_host.startMission(my_mission, my_clients, my_mission_record, 0, 'Moshe')
+                break
+            except RuntimeError as e:
+                if retry == max_retries - 1:
+                    print("Error starting mission", e)
+                    exit(1)
+                else:
+                    time.sleep(2)
+
+        world_state = self.agent_host.getWorldState()
+        print(f"init_malmo() - World State: {world_state}")
+        while not world_state.has_mission_begun:
+            # sys.stdout.write(".")
+            time.sleep(0.1)
+            world_state = self.agent_host.getWorldState()
+            for error in world_state.errors:
+                print("Error:", error.text)
+
+        self.agentJumpOffStartingBlock()
+        return world_state
+
+    def get_observation(self, world_state):
+        """
+
+        """
+        obs = np.zeros((self.numObservations,))
+        while world_state.is_mission_running:
+            world_state = self.agent_host.getWorldState()
+            if world_state.number_of_observations_since_last_state > 0:
+                # First we get the json from the observation API
+                msg = world_state.observations[-1].text
+                observations = json.loads(msg)
+                # print(f"get_observation() - observations: {observations}")
+
+        return obs
+
+    @staticmethod
+    def getPotentialActions(degreeChange=5):
+        """
             degreeChange: how many degrees we are allowed to change per angle, smaller degree = more angles
-            return: [(str,str)] of camera angles that the agent will be able to pick from as a discrete actions space
-        '''
-        out = []
-        for i in range(0,360,degreeChange):
-            for j in range(0,360,degreeChange):
-                out.append[(f"setYaw {i}",f"setPitch {j}")]
+            return: {0: (str,str), 1: (str, str)} of camera angles that the agent will be able to pick from
+                     as a discrete actions space
+        """
+        out = dict()
+        x = 0
+        for i in range(0, 360, degreeChange):
+            for j in range(0, 360, degreeChange):
+                out[x] = (f"setYaw {i}", f"setPitch {j}")
         return out
 
     def clearObservationVariables(self):
-        '''
+        """
         sets all temp observation variables(lastx, lasty, lastz, x,y,z velocity) to 0
-        '''
+        """
         self.lastx = 0
         self.lasty = 0
         self.lastz = 0
@@ -210,112 +258,29 @@ class elytraFlyer(gym.env):
         self.yvelocity = 0
         self.zvelocity = 0
 
+    def agentJumpOffStartingBlock(self):
+        self.agent_host.sendCommand("forward 1")
+        time.sleep(.1)
+        self.agent_host.sendCommand("jump 1")
+        time.sleep(.1)
+        self.agent_host.sendCommand("jump 0")
+        self.agent_host.sendCommand("forward 0")
+        time.sleep(.5)
+        print("before jump")
+        self.agent_host.sendCommand("jump 1")
+        print("after jump")
+        time.sleep(.3)
+        self.agent_host.sendCommand("jump 0")
 
 
-# Create default Malmo objects:
-agent_host = MalmoPython.AgentHost()
-try:
-    agent_host.parse( sys.argv )
-except RuntimeError as e:
-    print('ERROR:',e)
-    print(agent_host.getUsage())
-    exit(1)
-if agent_host.receivedArgument("help"):
-    print(agent_host.getUsage())
-    exit(0)
+if __name__ == '__main__':
+    ray.init()
+    trainer = ppo.PPOTrainer(env=elytraFlyer, config={
+        'env_config': {},           # No environment parameters to configure
+        'framework': 'torch',       # Use pyotrch instead of tensorflow
+        'num_gpus': 0,              # We aren't using GPUs
+        'num_workers': 0            # We aren't using parallelism
+    })
 
-if agent_host.receivedArgument("test"):
-    num_repeats = 1
-else:
-    num_repeats = 10
-
-for i in range(num_repeats):
-    size = int(6 + 0.5 * i)
-    print("Size of maze:", size)
-    my_mission = MalmoPython.MissionSpec(GetMissionXML("0", 0.4 + float(i / 20.0), size), True)
-    my_mission_record = MalmoPython.MissionRecordSpec()
-    my_mission.requestVideo(800, 500)
-    my_mission.setViewpoint(1)
-    # Attempt to start a mission:
-    max_retries = 3
-    my_clients = MalmoPython.ClientPool()
-    my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
-
-    for retry in range(max_retries):
-        try:
-            agent_host.startMission( my_mission, my_clients, my_mission_record, 0, "%s-%d" % ('Moshe', i) )
-            break
-        except RuntimeError as e:
-            if retry == max_retries - 1:
-                print("Error starting mission", (i+1), ":",e)
-                exit(1)
-            else:
-                time.sleep(2)
-
-    # Loop until mission starts:
-    print("Waiting for the mission", (i+1), "to start ",)
-    world_state = agent_host.getWorldState()
-    while not world_state.has_mission_begun:
-        #sys.stdout.write(".")
-        time.sleep(0.1)
-        world_state = agent_host.getWorldState()
-        for error in world_state.errors:
-            print("Error:",error.text)
-
-    print()
-    print("Mission", (i+1), "running.")
-
-    grid = load_grid(world_state)
-
-    #put calls to other functions here
-
-
-    # Action loop:
-    action_index = 0
-    initial_jump_complete = False
-    while world_state.is_mission_running:
-
-        # Tells agent to jump forward off starter platform
-        if not initial_jump_complete:
-            agent_host.sendCommand("forward 1")
-            time.sleep(.1)
-            agent_host.sendCommand("jump 1")
-            time.sleep(.1)
-            agent_host.sendCommand("jump 0")
-            agent_host.sendCommand("forward 0")
-            time.sleep(0.3)
-            agent_host.sendCommand("jump 1")
-            time.sleep(.1)
-            agent_host.sendCommand("jump 0")
-            initial_jump_complete = True
-
-        # Some example Yaw commands
-        agent_host.sendCommand("setYaw 0")
-        time.sleep(0.3)
-        agent_host.sendCommand("setYaw -50")
-        time.sleep(0.3)
-        agent_host.sendCommand("setYaw 60")
-
-        """
-        Stuff below this is probably not needed anymore but I don't want to delete yet 
-        just in case lol.
-        """
-        # Sending the next commend from the action list -- found using the Dijkstra algo.
-        action_list = []
-        if action_index >= len(action_list):
-            print("Error:", "out of actions, but mission has not ended!")
-            time.sleep(2)
-        else:
-            agent_host.sendCommand(action_list[action_index])
-        action_index += 1
-        if len(action_list) == action_index:
-            # Need to wait few seconds to let the world state realise I'm in end block.
-            # Another option could be just to add no move actions -- I thought sleep is more elegant.
-            time.sleep(2)
-        world_state = agent_host.getWorldState()
-        for error in world_state.errors:
-            print("Error:",error.text)
-
-    print()
-    print("Mission", (i+1), "ended")
-    # Mission has ended.
+    while True:
+        print(trainer.train())
